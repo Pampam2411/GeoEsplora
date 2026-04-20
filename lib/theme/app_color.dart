4c3ac26af98083a9f2d680ec8be0917adc7f5aaa
Luca <lucademarco2411@gmail.com>
2026-04-20 14:33:08 +0200

    add global color theme

---
+++ lib/theme/app_color.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color primary= Color(0xFF51A0BE);
+  static const Color primaryDark = Color(0xFF1F5D75);
+  static const Color secondary = Color(0xFF8A8175);
+  static const Color backgroundCard = Color(0xFFE8DCC8);
+  static const Color textPrimary = Color(0xFF2F2F2B);
+  static const Color greyLight = Color(0xFFD9D9D9);
+
+  static const Color background = Colors.white;
+  static const Color white = Colors.white;
+)
+}
