29c7527a5b1ab8f7bcec230de08bc3451b864ce1
Luca <lucademarco2411@gmail.com>
2026-04-21 10:28:32 +0200

    Unione modifiche

--- lib/theme/app_color.dart
+++ lib/theme/app_color.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class AppColors{
-  static const Color primary= Color(0xFF51A0BE);
+class AppColors {
+  static const Color primary = Color(0xFF51A0BE);
   static const Color primaryDark = Color(0xFF1F5D75);
   static const Color secondary = Color(0xFF8A8175);
   static const Color backgroundCard = Color(0xFFE8DCC8);
