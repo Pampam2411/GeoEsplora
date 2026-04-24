05589021ef030974db04f49e8b826e438773e269
Luca <lucademarco2411@gmail.com>
2026-04-24 09:59:59 +0200

    upload main

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:geoesplora/theme/app_theme.dart';
+import 'package:geoesplora/views/main_screen.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Geositi Calabria',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+
+      home: const MainScreen(),
+    );
+  }
+}
