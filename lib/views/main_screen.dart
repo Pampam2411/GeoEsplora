00eb07258b9a300331c837483f4423fe88bfea7a
Luca <lucademarco@gmail.com>
2026-04-24 18:21:34 +0200

    mod: modificato main screen per testare card

--- lib/views/main_screen.dart
+++ lib/views/main_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geoesplora/viewmodels/bottom_nav_viewmodel.dart';
 import 'package:geoesplora/widgets/custom_bottom_nav.dart';
 import 'package:geoesplora/widgets/custom_search_bar.dart';
+import 'package:geoesplora/widgets/geosito_card.dart';
 
 class MainScreen extends ConsumerWidget {
   const MainScreen({super.key});
@@ -12,13 +13,26 @@ class MainScreen extends ConsumerWidget {
     final currentIndex = ref.watch(bottomNavProvider);
 
     final List<Widget> pages = [
-      const Center(
-        child: Text('Schermata Home - Qui andrà la mappa dei geositi'),
+      Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            GeositoCard(
+              // Mettiamo un'immagine segnaposto a caso da internet per testare il layout
+              imageUrl:
+                  'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?q=80&w=1000&auto=format&fit=crop',
+              placeholder: 'Petra',
+              location: 'Cusé',
+            ),
+            const Center(child: Text('Schermata Mappa - Esplorazione')),
+            const Center(child: Text('Scheramata Fotocamera')),
+            const Center(child: Text('Schermata Geositi - Lista completa')),
+            const Center(
+              child: Text('Schermata Profilo - Impostazioni utente'),
+            ),
+          ],
+        ),
       ),
-      const Center(child: Text('Schermata Mappa - Esplorazione')),
-      const Center(child: Text('Scheramata Fotocamera')),
-      const Center(child: Text('Schermata Geositi - Lista completa')),
-      const Center(child: Text('Schermata Profilo - Impostazioni utente')),
     ];
 
     return Scaffold(
