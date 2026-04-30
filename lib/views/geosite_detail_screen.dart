7801a0cc57c898b51db2f43e84733244de501554
Luca <lucademarco2411@gmail.com>
2026-04-30 11:45:02 +0200

    feat:aggiunta riga con recensioni costo ecc, aggiunta slot per mappa, aggiunto slot immagini, aggiunto pulsante pianifica visita

--- lib/views/geosite_detail_screen.dart
+++ lib/views/geosite_detail_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:geoesplora/theme/app_color.dart';
 import 'package:geoesplora/widgets/detaild_tab_bar.dart';
+import 'package:geoesplora/widgets/favorite_button.dart';
+import 'package:geoesplora/widgets/geosite_map_preview.dart';
+import 'package:geoesplora/widgets/geosite_quick_info.dart';
 
 class GeositoDetailScreen extends StatefulWidget {
   const GeositoDetailScreen({super.key});
@@ -24,7 +28,7 @@ class _GeositoDetailScreenState extends State<GeositoDetailScreen> {
             top: 0,
             left: 0,
             right: 0,
-            height: screenHeight * 0.45,
+            height: screenHeight * 0.35,
             child: Stack(
               fit: StackFit.expand,
               children: [
@@ -47,49 +51,61 @@ class _GeositoDetailScreenState extends State<GeositoDetailScreen> {
                     ),
                   ),
                 ),
-              ],
-            ),
-          ),
-
-          Positioned(
-            top: screenHeight * 0.28,
-            left: 20,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Placeholder',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: 30.0,
+                    bottom: 30.0,
+                    top: 16,
+                    right: 16,
+                  ),
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: FavoriteButton(isInitiallyFavorite: false, size: 36),
                   ),
                 ),
-                const SizedBox(height: 5),
-                Row(
-                  children: [
-                    const Icon(
-                      Icons.location_on_outlined,
-                      color: Colors.white,
-                      size: 16,
-                    ),
-                    const SizedBox(width: 5),
-                    const Text(
-                      'Cosenza, Calabria',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400,
+
+                Positioned(
+                  left: 20,
+                  bottom: 60,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Placeholder',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                  ],
+                      const SizedBox(height: 5),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.location_on_outlined,
+                            color: Colors.white,
+                            size: 16,
+                          ),
+                          const SizedBox(width: 5),
+                          const Text(
+                            'Cosenza, Calabria',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
           ),
 
           Positioned(
-            top: screenHeight * 0.4,
+            top: screenHeight * 0.32,
             left: 0,
             right: 0,
             bottom: 0,
@@ -98,27 +114,34 @@ class _GeositoDetailScreenState extends State<GeositoDetailScreen> {
                 color: Colors.white,
                 borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
               ),
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    DetailTabBar(
-                      selectedIndex: _selectedTabIndex,
-                      onTabChanged: (index) {
-                        setState(() {
-                          _selectedTabIndex = index;
-                        });
-                      },
-                    ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  DetailTabBar(
+                    selectedIndex: _selectedTabIndex,
+                    onTabChanged: (index) {
+                      setState(() {
+                        _selectedTabIndex = index;
+                      });
+                    },
+                  ),
 
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: _selectedTabIndex == 0
-                          ? _buildDettagliNormali()
-                          : _buildDettagliScientifici(),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          top: 20.0,
+                          left: 20.0,
+                          right: 20.0,
+                          bottom: 40.0,
+                        ),
+                        child: _selectedTabIndex == 0
+                            ? _buildDettagliNormali()
+                            : _buildDettagliScientifici(),
+                      ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
@@ -131,16 +154,161 @@ class _GeositoDetailScreenState extends State<GeositoDetailScreen> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Text("Qui metteremo le icone: 3 ore, 1km, €€€..."),
-        const SizedBox(height: 20),
-        const Text(
-          "Dettagli",
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+        const GeositeQuickInfo(
+          durata: '3 ore',
+          distanza: '1km',
+          prezzo: '€€€',
+          rating: 4.8,
+        ),
+
+        const SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const Text(
+              "Dettagli",
+              style: TextStyle(
+                color: AppColors.textPrimary,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+
+            GestureDetector(
+              onTap: () => debugPrint("Apri recensione"),
+              child: const Text(
+                "143 recensioni",
+                style: TextStyle(
+                  color: AppColors.secondary,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ],
+        ),
+
+        const SizedBox(height: 10),
+
+        Text(
+          "Lorem ipsum dolor sit ....",
+          style: TextStyle(
+            color: AppColors.secondary,
+            fontWeight: FontWeight.w400,
+            fontSize: 13,
+          ),
+        ),
+
+        const SizedBox(height: 10),
+        Row(
+          children: [
+            GestureDetector(
+              onTap: () => debugPrint("Espandi testo"),
+              child: const Text(
+                "Leggi di più",
+                style: TextStyle(
+                  color: AppColors.secondary,
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+            const SizedBox(width: 20),
+            GestureDetector(
+              onTap: () => debugPrint("Apri accessibilità"),
+              child: const Text(
+                "Informazioni sull'accessibilità",
+                style: TextStyle(
+                  color: AppColors.secondary,
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ],
         ),
         const SizedBox(height: 10),
-        Text(
-          "Lorem ipsum dolor sit amet...",
-          style: TextStyle(color: Colors.grey.shade600),
+
+        const GeositeMapPreview(),
+
+        const SizedBox(height: 10),
+
+        //IMMAGINI
+        const Text(
+          "Immagini",
+          style: TextStyle(
+            color: AppColors.textPrimary,
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+        ),
+        const SizedBox(height: 10),
+
+        SizedBox(
+          height: 100,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 4,
+            itemBuilder: (context, index) {
+              return Container(
+                width: 140,
+                margin: const EdgeInsets.only(right: 15),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.grey.shade300,
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.network(
+                    'https://images.unsplash.com/photo-1542224566-6e85f2e6772f',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+
+        const SizedBox(height: 10),
+        //PULSANTE PIANIFICA VISITA
+        SizedBox(
+          width: double.infinity,
+          height: 54,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.secondary,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              elevation: 0,
+            ),
+            onPressed: () {
+              debugPrint("Vai alla schermata di pianificazione!");
+            },
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Pianifica visita",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 10),
+                FaIcon(
+                  FontAwesomeIcons.locationArrow,
+                  color: AppColors.white,
+                  size: 20,
+                ),
+              ],
+            ),
+          ),
         ),
       ],
     );
@@ -157,7 +325,7 @@ class _GeositoDetailScreenState extends State<GeositoDetailScreen> {
         const SizedBox(height: 10),
         Text(
           "Qui andranno le ere geologiche, tipo di roccia,ecc...",
-          style: TextStyle(color: Colors.grey.shade600),
+          style: TextStyle(color: AppColors.grey),
         ),
       ],
     );
