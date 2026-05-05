fc7f76852287ba01821c03082f79e0ed387cb689
Luca <lucademarco2411@gmail.com>
2026-05-05 11:47:58 +0200

    mod: modificato main screen per visualizzare le varie schermate

--- lib/views/main_screen.dart
+++ lib/views/main_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:geoesplora/views/dashboard_view.dart';
 import 'package:geoesplora/views/geosite_detail_screen.dart';
-import 'package:geoesplora/widgets/rock_card.dart';
+import 'package:geoesplora/views/home_view.dart';
 import 'package:geoesplora/viewmodels/bottom_nav_viewmodel.dart';
+import 'package:geoesplora/views/rock_list_view.dart';
 import 'package:geoesplora/widgets/custom_bottom_nav.dart';
 import 'package:geoesplora/widgets/custom_search_bar.dart';
-import 'package:geoesplora/widgets/geosite_card.dart';
 
 class MainScreen extends ConsumerWidget {
   const MainScreen({super.key});
@@ -15,35 +16,11 @@ class MainScreen extends ConsumerWidget {
     final currentIndex = ref.watch(bottomNavProvider);
 
     final List<Widget> pages = [
-      GeositoDetailScreen(),
-      Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            GeositeCard(
-              imageUrl:
-                  'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?q=80&w=1000&auto=format&fit=crop',
-              placeholder: 'Placeholder',
-              location: 'Cosenza',
-              rating: 4.8,
-              reviewCount: 143,
-            ),
-            RockCard(
-              imageUrl:
-                  'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?q=80&w=1000&auto=format&fit=crop',
-              rockName: 'Marmo',
-              type: 'Sedimentaria',
-            ),
-            const Center(child: Text('Schermata Mappa - Esplorazione')),
-
-            const Center(child: Text('Scheramata Fotocamera')),
-            const Center(child: Text('Schermata Geositi - Lista completa')),
-            const Center(
-              child: Text('Schermata Profilo - Impostazioni utente'),
-            ),
-          ],
-        ),
-      ),
+      const DashboardView(),
+      const HomeView(),
+      const GeositoDetailScreen(),
+      const RockListView(),
+      const Center(child: Text('Schermata Profilo')),
     ];
 
     return Scaffold(
