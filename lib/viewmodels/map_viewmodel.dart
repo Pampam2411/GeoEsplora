f910d9b64ecfe98ee996217ff280ee1d7c832a79
Luca <lucademarco@gmail.com>
2026-06-09 17:40:27 +0200

    feat: aggiunti lista geositi alla mappa, aggiunto posizionamento dei geositi sulla mappa.

--- lib/viewmodels/map_viewmodel.dart
+++ lib/viewmodels/map_viewmodel.dart
@@ -4,9 +4,30 @@ import 'package:latlong2/latlong.dart';
 
 class MapViewModel extends ChangeNotifier {
   final MapController mapController = MapController();
+  final PageController pageController = PageController(viewportFraction: 0.85);
+  int selectedIndex = 0;
 
   final LatLng initialCenter = const LatLng(39.3592, 16.2263);
   final double initialZoom = 9.0;
+
+  void onMarkerTapped(int index, LatLng location) {
+    if (selectedIndex == index) return;
+
+    selectedIndex = index;
+    mapController.move(location, 14.0);
+    pageController.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+    notifyListeners();
+  }
+
+  void onPageChanged(int index, LatLng location) {
+    selectedIndex = index;
+    mapController.move(location, 14.0);
+    notifyListeners();
+  }
 
   void resetCamera() {
     mapController.move(initialCenter, initialZoom);
@@ -15,6 +36,7 @@ class MapViewModel extends ChangeNotifier {
   @override
   void dispose() {
     mapController.dispose();
+    pageController.dispose();
     super.dispose();
   }
 }
--- lib/views/map_view.dart
+++ lib/views/map_view.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:geoesplora/theme/app_color.dart';
 import 'package:geoesplora/viewmodels/map_viewmodel.dart';
+import 'package:geoesplora/widgets/cards/geosite_card.dart';
 import 'package:geoesplora/widgets/sections/geosite_map_preview.dart';
 import 'package:geoesplora/widgets/utils/fade_gradient.dart';
 import 'package:geoesplora/widgets/inputs/custom_search_bar.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geoesplora/viewmodels/geosite_list_viewmodel.dart';
 
 class MapView extends ConsumerStatefulWidget {
   const MapView({super.key});
@@ -33,11 +36,14 @@ class _MapViewState extends ConsumerState<MapView> {
   Widget build(BuildContext context) {
     ref.listen<LatLng?>(targetMapLocationProvider, (previous, next) {
       if (next != null) {
-        _viewModel.mapController.move(next, 16.0);
+        _viewModel.mapController.move(next, 14.0);
 
         ref.read(targetMapLocationProvider.notifier).state = null;
       }
     });
+
+    final geosites = ref.watch(geositeListProvider);
+
     return Scaffold(
       backgroundColor: AppColors.background,
       body: Stack(
@@ -51,10 +57,41 @@ class _MapViewState extends ConsumerState<MapView> {
                 flags: InteractiveFlag.all & ~InteractiveFlag.rotate,
               ),
             ),
+
             children: [
               TileLayer(
+                keepBuffer: 2,
+                panBuffer: 1,
+                maxNativeZoom: 19,
+                maxZoom: 19,
                 urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                 userAgentPackageName: 'com.example.geoesplora',
+              ),
+              MarkerLayer(
+                markers: geosites.asMap().entries.map((entry) {
+                  final index = entry.key;
+                  final geosite = entry.value;
+                  final isSelected = _viewModel.selectedIndex == index;
+                  return Marker(
+                    point: LatLng(geosite.latitude, geosite.longitude),
+                    width: isSelected ? 65 : 50,
+                    height: isSelected ? 65 : 50,
+                    alignment: Alignment.topCenter,
+                    child: GestureDetector(
+                      onTap: () {
+                        _viewModel.onMarkerTapped(
+                          index,
+                          LatLng(geosite.latitude, geosite.longitude),
+                        );
+                      },
+                      child: const FaIcon(
+                        FontAwesomeIcons.locationDot,
+                        color: AppColors.primary,
+                        size: 36,
+                      ),
+                    ),
+                  );
+                }).toList(),
               ),
             ],
           ),
@@ -70,7 +107,7 @@ class _MapViewState extends ConsumerState<MapView> {
             ),
           ),
 
-          // Search Bar
+          // SEARCH BAR
           SafeArea(
             bottom: false,
             child: Padding(
@@ -80,6 +117,40 @@ class _MapViewState extends ConsumerState<MapView> {
               ),
               child: CustomSearchBar(),
             ),
+          ),
+
+          //LISTA GEOSITECARD
+          AnimatedBuilder(
+            animation: _viewModel,
+            builder: (context, child) {
+              return Positioned(
+                bottom: 110,
+                left: 0,
+                right: 0,
+                height: 220,
+                child: PageView.builder(
+                  controller: _viewModel.pageController,
+                  itemCount: geosites.length,
+                  onPageChanged: (index) {
+                    final geosite = geosites[index];
+                    _viewModel.onPageChanged(
+                      index,
+                      LatLng(geosite.latitude, geosite.longitude),
+                    );
+                  },
+                  itemBuilder: (context, index) {
+                    final geosite = geosites[index];
+                    final isSelected = _viewModel.selectedIndex == index;
+
+                    return AnimatedScale(
+                      duration: const Duration(milliseconds: 250),
+                      scale: isSelected ? 1.0 : 0.9,
+                      child: GeositeCard(geosite: geosite, showShadow: true),
+                    );
+                  },
+                ),
+              );
+            },
           ),
         ],
       ),
